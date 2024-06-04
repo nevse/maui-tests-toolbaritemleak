e834a6aec6ec16fcd5545386124de3e86c2092a6
nevse <nov.eugen@gmail.com>
2024-06-04 10:29:57 +0400

    add example with memory leaks in toolbar item

---
+++ LeakPage.xaml.cs
@@ -0,0 +1,13 @@
+namespace ToolbarItemLeak;
+
+public partial class LeakPage : ContentPage
+{
+	public LeakPage()
+	{
+		InitializeComponent();
+	}
+	void OnSomeItemClicked(object sender, EventArgs e)
+	{
+		// Do something
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace ToolbarItemLeak;
+
+public partial class MainPage : ContentPage
+{
+	WeakReference<Page> pageReference;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void ShowPageWithLeak(object sender, EventArgs e)
+    {
+		var leakPage = new LeakPage();
+		pageReference = new WeakReference<Page>(leakPage);
+		Navigation.PushAsync(leakPage);
+    }
+	void CheckLeak(object sender, EventArgs e)
+	{
+		GC.Collect();
+		GC.WaitForPendingFinalizers();
+		GC.Collect();
+		if (pageReference.TryGetTarget(out var page))
+		{
+			DisplayAlert("Leak", "LeakPage is still alive", "OK");
+		}
+		else
+		{
+			DisplayAlert("No Leak", "LeakPage is gone", "OK");
+		}
+	}
+}
+
